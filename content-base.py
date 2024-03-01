30ec603ea6bf7bfdb56a61300c4b279a0871a245
pfigithub <pampam7072@gmail.com>
2024-03-01 11:37:14 +0330

    reading data

--- content-base.py
+++ content-base.py
@@ -1,3 +1,10 @@
 # importing packages
 import pandas as pd
 import numpy as np
+
+#Storing the movie information into a pandas dataframe
+movies_df = pd.read_csv('movies.csv')
+#Storing the user information into a pandas dataframe
+ratings_df = pd.read_csv('ratings.csv')
+#Head is a function that gets the first N rows of a dataframe. N's default is 5.
+movies_df.head()
