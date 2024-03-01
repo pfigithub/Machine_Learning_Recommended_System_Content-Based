1a979466634c2ac57ff314f327c117f939540725
pfigithub <pampam7072@gmail.com>
2024-03-01 11:38:49 +0330

    pre-processing

--- content-base.py
+++ content-base.py
@@ -8,3 +8,48 @@ movies_df = pd.read_csv('movies.csv')
 ratings_df = pd.read_csv('ratings.csv')
 #Head is a function that gets the first N rows of a dataframe. N's default is 5.
 movies_df.head()
+
+#Using regular expressions to find a year stored between parentheses
+#We specify the parantheses so we don't conflict with movies that have years in their titles
+movies_df['year'] = movies_df.title.str.extract('(\(\d\d\d\d\))',expand=False)
+#Removing the parentheses
+movies_df['year'] = movies_df.year.str.extract('(\d\d\d\d)',expand=False)
+#Removing the years from the 'title' column
+movies_df['title'] = movies_df.title.str.replace('(\(\d\d\d\d\))', '')
+#Applying the strip function to get rid of any ending whitespace characters that may have appeared
+movies_df['title'] = movies_df['title'].apply(lambda x: x.strip())
+#Every genre is separated by a | so we simply have to call the split function on |
+movies_df['genres'] = movies_df.genres.str.split('|')
+
+#Copying the movie dataframe into a new one since we won't need to use the genre information in our first case.
+moviesWithGenres_df = movies_df.copy()
+
+#For every row in the dataframe, iterate through the list of genres and place a 1 into the corresponding column
+for index, row in movies_df.iterrows():
+    for genre in row['genres']:
+        moviesWithGenres_df.at[index, genre] = 1
+#Filling in the NaN values with 0 to show that a movie doesn't have that column's genre
+moviesWithGenres_df = moviesWithGenres_df.fillna(0)
+
+#Drop removes a specified row or column from a dataframe
+ratings_df = ratings_df.drop('timestamp', 1)
+ratings_df.head()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
